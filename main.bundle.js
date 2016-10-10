26466c2b8df8bdcf721de50b4d81920910d73868
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-10 17:12:44 -0600

    add blocks to canvas
    close #1 add background blocks.

--- main.bundle.js
+++ main.bundle.js
@@ -44,7 +44,10 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	
+	var canvas = document.getElementById('game');
+	var context = canvas.getContext('2d');
+
+	context.fillRect(50, 50, 10, 10);
 
 /***/ }
 /******/ ]);
