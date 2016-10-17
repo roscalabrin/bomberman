d07ebf43747d4a603102d0505c4d7a146207bf3a
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-17 13:37:12 -0600

    close #44 add update score function for when the game ends

---
+++ test/score.js
@@ -0,0 +1,14 @@
+const Game = require('./../lib/game')
+const stub = require('./support/stub')
+const assert = require('chai').assert
+const Score = require('./../lib/score')
+const canvas = { width: 1470, height: 770 }
+
+describe('Score', () => {
+  context('properties', () => {
+    it('has a initial score for each player', () => {
+      const initialScore = new Score()
+      assert.equal(initialScore.firstPlayerScore, 0)
+    }
+  }
+}
